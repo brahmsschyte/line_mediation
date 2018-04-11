60470721f572eea8b7403516cde2ea5c0cbe626e
Bramanthyo Andrian <brahms.schyte@gmail.com>
2018-04-11 00:24:11 -0700

    sms

---
+++ app/controllers/sms_webhook_controller.rb
@@ -0,0 +1,60 @@
+class SmsWebhookController < ApplicationController
+  after_action :set_retrieved_flag, only: [:index]
+
+  def callback # from/to cyclops
+    unless is_validate_signature
+      head :bad_request
+    end
+
+    sms_params[:msisdn].each do |n|
+      uri = URI(secrets.sms_url)
+      par = { :cp_name => secrets.sms_cp_name, :pwd => secrets.sms_pwd, :msisdn => n, :sms => sms_params[:content] }
+      uri.query = URI.encode_www_form(par)
+
+      res = Net::HTTP.get_response(uri)
+      if res.is_a?(Net::HTTPSuccess)
+        res_body = res.body.strip.split(':')
+        SmsDeliveryReport.create(msisdn: n, trx_id: res_body[1], trx_stat: res_body[0])
+      end
+    end
+
+    head :ok
+  end
+
+  def delivery_report # from/to tsel smsc
+    @sms = SmsDeliveryReport.find_by(trx_id: delivery_report_params[:trxid])
+    @sms.update(status: delivery_report_params[:status]) unless @sms.blank?
+
+    head :ok
+  end
+
+  def index
+    @sms = SmsDeliveryReport.where(retrieved: false)
+
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  private
+
+  def sms_params
+    params.permit(:content, msisdn: [])
+  end
+
+  def delivery_report_params
+    params.permit(:trxid, :status)
+  end
+
+  def set_retrieved_flag
+    @sms.update_all(retrieved: true)
+  end
+
+  def secrets
+    @secrets = Rails.application.secrets
+  end
+
+  def is_validate_signature
+    params[:key] == secrets[:sms_key]
+  end
+end
--- db/migrate/20180328023910_create_sms_delivery_reports.rb
+++ db/migrate/20180328023910_create_sms_delivery_reports.rb
@@ -1,8 +1,11 @@
 class CreateSmsDeliveryReports < ActiveRecord::Migration[5.1]
   def change
     create_table :sms_delivery_reports do |t|
+      t.string :msisdn
       t.string :trx_id
+      t.string :trx_stat
       t.string :status
+      t.boolean :retrieved
 
       t.timestamps
     end
