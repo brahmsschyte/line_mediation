634fa26c64a18b21a88a15fef9fc898306ede807
Bramanthyo Andrian <brahms.schyte@gmail.com>
2018-04-11 01:59:36 -0700

    sms bugfix

--- app/admin/sms_delivery_report.rb
+++ app/admin/sms_delivery_report.rb
@@ -4,13 +4,15 @@ ActiveAdmin.register SmsDeliveryReport do
   index do
     selectable_column
     id_column
+    column :msisdn
     column :trx_id
-    column :status
+    column :trx_stat
     column :updated_at
     actions
   end
 
+  filter :msisdn
   filter :trx_id
-  filter :status
+  filter :trx_stat
   filter :updated_at
 end
