c18ecc1da2e3e471a152d89031e749c31489c549
Bramanthyo Andrian <brahms.schyte@gmail.com>
2018-08-22 05:33:02 -0700

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   post 'line/:key/callback',  to: 'line_webhook#callback'
   post 'mogi/:key/callback',  to: 'mogi_webhook#callback'
-  post 'mbp/:key/callback',   to: 'mogi_webhook#callback'
+  post 'mbp/:key/callback',   to: 'mbp_webhook#callback'
   post 'sms/:key/callback',   to: 'sms_webhook#callback'
   get  'sms/delivery_report', to: 'sms_webhook#delivery_report'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
